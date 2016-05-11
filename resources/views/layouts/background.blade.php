aa756e4a36c9f15a59f5e9e134ea41e65ee5c780
ebo-one <e@ebo.one>
2016-05-11 09:39:04 -0400

    Create include for custom page styling

---
+++ resources/views/layouts/background.blade.php
@@ -0,0 +1,15 @@
+<style>
+    body {
+        background: url(../img/clock-bkgrnd.jpg) no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+
+    @media only screen and (max-width: 640px) {
+    body {
+            background: #DBCABA;
+        }
+    }
+</style>
