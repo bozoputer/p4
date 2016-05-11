2cb6ba67b23115b367be66629bfda95b0737c90c
ebo-one <e@ebo.one>
2016-05-11 09:38:22 -0400

    Add Blade include for custom styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,7 @@
 @extends('layouts.master')
 
 @section('head')
-<style>
-    body {
-        background: url(../img/clock-bkgrnd.jpg) no-repeat center center fixed;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
-    }
-
-    @media only screen and (max-width: 640px) {
-    body {
-            background: #DBCABA;
-        }
-    }
-</style>
+    @include('layouts.background')
 @stop
 
 @section('content')
