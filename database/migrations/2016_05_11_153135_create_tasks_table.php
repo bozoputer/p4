386f291c74a8bd23b369adeba82ff08e67c2062d
ebo-one <e@ebo.one>
2016-05-11 16:36:46 -0400

    Add create tasks table

---
+++ database/migrations/2016_05_11_153135_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+
+        $table->increments('id');
+
+        $table->integer('list_id');
+        $table->string('task_name');
+        $table->string('note');
+        $table->boolean('status');
+
+        $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
