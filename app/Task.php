90122901198b68bac4c70a23e9728d8db0b38aa3
ebo-one <e@ebo.one>
2016-05-12 23:22:24 -0400

    Add more fields for mass assignment

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['task'];
+    protected $fillable = ['task', 'list_id'];
 }
--- app/Tasklist.php
+++ app/Tasklist.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tasklist extends Model
 {
-    protected $fillable = ['list_name', 'user_id'];
+    protected $fillable = ['list_name', 'user_id', 'id'];
 }
